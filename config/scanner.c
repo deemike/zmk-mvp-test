1f11af605f919baca089ff6d311dbde5e3842aff
deemike <deemike@seznam.cz>
2026-06-07 20:39:37 +0200

    Refactor scanner thread to send commands in a loop
    Removed unnecessary sleep and added a loop to send commands every second.

--- config/scanner.c
+++ config/scanner.c
@@ -2,7 +2,6 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/uart.h>
 
-/* Выделяем сканеру собственную память и создаем независимый поток */
 #define SCANNER_STACK_SIZE 1024
 #define SCANNER_PRIORITY 7
 
@@ -13,11 +12,6 @@ void scanner_thread_func(void *arg1, void *arg2, void *arg3) {
         return;
     }
 
-    /* Ждем 2 полных секунды! Даем ZMK спокойно запустить USB и Bluetooth, 
-       а сканеру - полностью проснуться после подачи питания */
-    k_msleep(2000);
-
-    /* Магический пакет: Включить синее кольцо */
     uint8_t cmd[] = {
         0xEF, 0x01, 0xFF, 0xFF, 0xFF, 0xFF, 
         0x01, 0x00, 0x07,                   
@@ -25,12 +19,15 @@ void scanner_thread_func(void *arg1, void *arg2, void *arg3) {
         0x00, 0x42                          
     };
 
-    for (int i = 0; i < sizeof(cmd); i++) {
-        uart_poll_out(uart_dev, cmd[i]);
+    /* Бесконечный цикл: стучимся в сканер каждую секунду */
+    while (1) {
+        for (int i = 0; i < sizeof(cmd); i++) {
+            uart_poll_out(uart_dev, cmd[i]);
+        }
+        k_msleep(1000); /* Спим 1 секунду и повторяем */
     }
 }
 
-/* Ядро Zephyr само запустит этот поток параллельно клавиатуре */
 K_THREAD_DEFINE(scanner_tid, SCANNER_STACK_SIZE,
                 scanner_thread_func, NULL, NULL, NULL,
                 SCANNER_PRIORITY, 0, 0);
